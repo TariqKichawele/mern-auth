2a66f8dd686f3b95ec2b33525dcff21158c81d8a
TariqKichawele <tariqkichawele66@gmail.com>
2024-02-14 10:08:55 -0500

    connected to mongoDB

--- server/index.js
+++ server/index.js
@@ -1,7 +1,19 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
+dotenv.config();
 const app = express();
 
 app.listen(3005, () => {
     console.log('Hello World!!');
+});
+
+mongoose
+.connect(process.env.MONGO_URI)
+.then(() => {
+    console.log('DB Connected');
+})
+.catch((err) => {
+    console.log(err);
 });
