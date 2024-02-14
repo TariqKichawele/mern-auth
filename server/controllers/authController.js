8ac29b94a7eb8065074c56f7403228bace0014a7
TariqKichawele <tariqkichawele66@gmail.com>
2024-02-14 17:21:18 -0500

    complete sign in api route

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,5 +1,7 @@
 import User from "../models/userModel.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signUp = async (req, res, next) => {
     const { username, email, password } = req.body;
@@ -18,4 +20,28 @@ export const signUp = async (req, res, next) => {
         next(error);
     }
 
+};
+
+export const signIn = async (req, res, next) => {
+    const { email, password } = req.body;
+    try {
+        const validUser = await User.findOne({ email }); 
+        if (!validUser) return next(errorHandler("Invalid email or password", 401));
+
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if (!validPassword) return next(errorHandler("Invalid email or password", 401));
+
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET );
+        const { password: hashedPassword, ...rest } = validUser._doc;
+
+        const expiryDate = new Date(Date.now() + 3600000);
+        
+        res.cookie("access_token", token, {
+            httpOnly: true,
+            expires: expiryDate,
+        }).status(200).json(rest);
+        
+    } catch (error) {
+        next(error);
+    }
 };
